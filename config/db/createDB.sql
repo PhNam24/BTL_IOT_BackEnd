471dbb9da5a8726f83917ba21b4a5e702b5c1ee9
PhNam24 <phamnam240803@gmail.com>
2024-11-17 14:10:34 +0700

    update sql scritps

--- config/db/createDB.sql
+++ config/db/createDB.sql
@@ -5,7 +5,58 @@ CREATE TABLE User (
     email VARCHAR(100),
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    attribute VARCHAR(50) -- Để lưu role (admin/user)
+    attribute VARCHAR(50) -- Lưu vai trò (admin/user)
+);
+
+-- Tạo bảng Fan
+CREATE TABLE Fan (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    status INT
+);
+
+-- Tạo bảng Speaker
+CREATE TABLE Speaker (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    status INT
+);
+
+-- Tạo bảng Sensor
+CREATE TABLE Sensor (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    status INT
+);
+
+-- Tạo bảng Led
+CREATE TABLE Led (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    des VARCHAR(255),
+    status INT,
+    led_brightness INT,
+    led_text_size INT
+);
+
+-- Tạo bảng Device
+CREATE TABLE Device (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    sensor_id INT,
+    fan_id INT,
+    speaker_id INT,
+    note VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES User(id),
+    FOREIGN KEY (sensor_id) REFERENCES Sensor(id),
+    FOREIGN KEY (fan_id) REFERENCES Fan(id),
+    FOREIGN KEY (speaker_id) REFERENCES Speaker(id)
+);
+
+-- Tạo bảng History
+CREATE TABLE History (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    humidity FLOAT,
+    temperature FLOAT,
+    timestamp DATETIME,
+    sensor_id INT,
+    FOREIGN KEY (sensor_id) REFERENCES Sensor(id)
 );
 
 -- Tạo bảng Setting
@@ -15,33 +66,6 @@ CREATE TABLE Setting (
     upper_humid FLOAT,
     lower_temp FLOAT,
     upper_temp FLOAT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(id)
+    device_id INT,
+    FOREIGN KEY (device_id) REFERENCES Device(id)
 );
-
--- Tạo bảng Led
-CREATE TABLE Led (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    des TEXT,
-    status INTEGER,
-    led_brightness INTEGER,
-    led_text_size INTEGER
-);
-
--- Tạo bảng History
-CREATE TABLE History (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    humidity FLOAT,
-    temperature FLOAT,
-    timestamp DATETIME
-);
-
--- Tạo bảng Sensor
-CREATE TABLE Sensor (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    status INTEGER
-);
-
--- Thêm tài khoản admin vào bảng User
-INSERT INTO User ( name, email, username, password, attribute)
-VALUES ('admin', NULL, 'admin', 'iotproject123', 'admin');
