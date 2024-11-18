b7a28a56d336563fab3ff93dc2cc032dbd34e5b7
Bui Huy Hoang <buihuyhoang2709.dev@gmail.com>
2024-11-18 09:26:50 +0700

    Fix authen

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cor = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
-const SensorController = require('./controller/SensorController');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cor = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+const SensorController = require("./controller/SensorController");
 
-const {client: mqttClient, connect, message} = require('./config/mqtt/mqtt');
+const { client: mqttClient, connect, message } = require("./config/mqtt/mqtt");
 
 const app = express();
 const port = 3000;
@@ -14,26 +14,31 @@ const port = 3000;
 connect();
 message();
 app.use((req, res, next) => {
-    // Publish messages
-    req.mqttPublish = function (topic, message) {
-        mqttClient.publish(topic, message)
-    }
+  // Publish messages
+  req.mqttPublish = function (topic, message) {
+    mqttClient.publish(topic, message);
+  };
 
-    // Subscribe to topic
-    req.mqttSubscribe = function (topic, callback) {
-        mqttClient.subscribe(topic)
-        mqttClient.on('message', function (t, m) {
-            if (t === topic) {
-                callback(m.toString())
-            }
-        })
-    }
-    next()
-})
+  // Subscribe to topic
+  req.mqttSubscribe = function (topic, callback) {
+    mqttClient.subscribe(topic);
+    mqttClient.on("message", function (t, m) {
+      if (t === topic) {
+        callback(m.toString());
+      }
+    });
+  };
+  next();
+});
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cor());
+app.use(
+  cor({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use("/user", require("./routes/user"));
 app.use("/device", require("./routes/device"));
 app.use("/led", require("./routes/led"));
@@ -44,56 +49,56 @@ app.use("/sensor", require("./routes/sensor"));
 const server = http.createServer(app);
 const io = socketIo(server);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('getNewestData', (sensorId) => {
-        SensorController.getNewestData(sensorId, (err, data) => {
-            if (err) {
-                socket.emit('error', 'Error getting newest data: ' + err.message);
-            } else {
-                socket.emit('newestData', data);
-            }
-        });
+  socket.on("getNewestData", (sensorId) => {
+    SensorController.getNewestData(sensorId, (err, data) => {
+      if (err) {
+        socket.emit("error", "Error getting newest data: " + err.message);
+      } else {
+        socket.emit("newestData", data);
+      }
     });
+  });
 
-    socket.on('getHistoryData', (sensorId) => {
-        SensorController.getHistoryData(sensorId, (err, data) => {
-            if (err) {
-                socket.emit('error', 'Error getting history data: ' + err.message);
-            } else {
-                socket.emit('historyData', data);
-            }
-        });
+  socket.on("getHistoryData", (sensorId) => {
+    SensorController.getHistoryData(sensorId, (err, data) => {
+      if (err) {
+        socket.emit("error", "Error getting history data: " + err.message);
+      } else {
+        socket.emit("historyData", data);
+      }
     });
+  });
 
-    // Thêm các sự kiện cho turnOnSensor và turnOffSensor
-    socket.on('turnOnSensor', (sensorId) => {
-        SensorController.turnOnSensor(sensorId, (err, result) => {
-            if (err) {
-                socket.emit('error', 'Error turning on sensor: ' + err.message);
-            } else {
-                socket.emit('sensorStatusChanged', { sensorId, status: "ON" });
-            }
-        });
+  // Thêm các sự kiện cho turnOnSensor và turnOffSensor
+  socket.on("turnOnSensor", (sensorId) => {
+    SensorController.turnOnSensor(sensorId, (err, result) => {
+      if (err) {
+        socket.emit("error", "Error turning on sensor: " + err.message);
+      } else {
+        socket.emit("sensorStatusChanged", { sensorId, status: "ON" });
+      }
     });
+  });
 
-    socket.on('turnOffSensor', (sensorId) => {
-        SensorController.turnOffSensor(sensorId, (err, result) => {
-            if (err) {
-                socket.emit('error', 'Error turning off sensor: ' + err.message);
-            } else {
-                socket.emit('sensorStatusChanged', { sensorId, status: "OFF" });
-            }
-        });
+  socket.on("turnOffSensor", (sensorId) => {
+    SensorController.turnOffSensor(sensorId, (err, result) => {
+      if (err) {
+        socket.emit("error", "Error turning off sensor: " + err.message);
+      } else {
+        socket.emit("sensorStatusChanged", { sensorId, status: "OFF" });
+      }
     });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 // Start server
 server.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
